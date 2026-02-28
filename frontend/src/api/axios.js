0756a21bdb79c6efbc249c9801d92833f8efcad9
Sadaf Fatima <144591833+sadaf-fatima100@users.noreply.github.com>
2026-02-28 01:07:15 -0900

    Update axios.js with backend live link

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://hirenova-gt0t.onrender.com/api/v1",
   withCredentials: true,
 });
 
